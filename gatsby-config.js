7d9d49602056d724279a89a5b8c4db91c0047649
aymen ourabi <aymen.ourabi@digit-u.com>
2019-11-22 16:36:55 +0100

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,13 +5,19 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+   
     `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-json`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
+      options: {
+        name: `project`,
+        path: `${__dirname}/src/data`,
+      }
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
